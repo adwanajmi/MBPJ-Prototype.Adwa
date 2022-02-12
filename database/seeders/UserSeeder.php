b827d132b2487edca0b0dc5f4f01106bc4aa5dc0
mushroom <syafiqahalim.work@gmail.com>
2022-02-12 07:11:44 +0000

    Add seeders and new migration

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +14,16 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $users = [
+            [
+                'id'                 => 1,
+                'name'               => 'Admin',
+                'email'              => 'admin@admin.com',
+                'password'           => bcrypt('password'),
+                'remember_token'     => null,
+            ],
+        ];
+
+        User::insert($users);
     }
 }
